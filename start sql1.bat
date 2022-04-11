b096f8bdc1ca9482a025a64106140465e2dbb886
Avinash Kumar <102034923+avinashexpt@users.noreply.github.com>
2022-04-11 13:21:55 +0530

    Add files via upload

---
+++ start sql1.bat
@@ -0,0 +1,3 @@
+cd C:\Users\PC-22\Downloads\hsqldb-2.6.1\hsqldb-2.6.1\hsqldb\lib
+java -cp hsqldb.jar org.hsqldb.server.Server --database.0 file:mydb --dbname.0 xdb
+
---
+++ start sql2.bat
@@ -0,0 +1,2 @@
+cd C:\Users\PC-22\Downloads\hsqldb-2.6.1\hsqldb-2.6.1\hsqldb\lib
+java -jar hsqldb.jar
